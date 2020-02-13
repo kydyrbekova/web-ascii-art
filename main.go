6de54bc548c78b19828b5ca78a4a02bdc675dc38
kydyrbekova <a.kydyrbekova@gmail.com>
2020-02-14 01:29:23 +0600

    add files

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var text, fonts, Result string
+
+func main() {
+	http.HandleFunc("/", handlefunc)
+	fmt.Println("Listening to port :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handlefunc(w http.ResponseWriter, request *http.Request) {
+	if request.URL.Path != "/" {
+		http.Error(w, "Error 404\n Page not Found", http.StatusNotFound)
+		return
+	}
+
+	switch request.Method {
+	case "GET":
+		http.ServeFile(w, request, "html")
+	case "POST":
+		fonts := request.FormValue("fonts")
+		text := request.FormValue("text")
+
+		for _, e := range text {
+			if e < 32 || e > 126 {
+				http.Error(w, "Error 400\n Bad Request", http.StatusNotFound)
+				return
+			}
+		}
+		file, err := os.Open("./" + fonts + ".txt")
+
+		if err != nil {
+			http.Error(w, "Internal Server error - 500", http.StatusNotFound)
+		}
+
+		fileContent := ScanFile(file)
+		arg := strings.Split(text+" ", "\\n")
+
+		for _, val := range arg {
+			asciiResult(string(val), fileContent)
+		}
+
+		temp, _ := template.ParseFiles("html/index.html")
+		temp.Execute(w, Result)
+	}
+}
+
+func asciiResult(s string, fileContent []string) string {
+	for i := 1; i <= 8; i++ {
+		for _, arg := range s {
+			indexBase := int(rune(arg)-32) * 9
+			Result = Result + string(fileContent[indexBase+i])
+		}
+		Result = Result + "\n"
+	}
+	return Result
+}
+
+func ScanFile(font *os.File) []string {
+	Result = ""
+	var fileContent []string
+	scanner := bufio.NewScanner(font)
+	for scanner.Scan() {
+		lines := scanner.Text()
+		fileContent = append(fileContent, lines)
+	}
+	return fileContent
+}
